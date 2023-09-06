a83bc0c0ea67df447e8c7fa8865428f22b29d0e9
A. Shustovsky <a.shustovsky@gmail.com>
2023-09-06 16:02:13 +0400

    refactor: add mock data

---
+++ src/assets/mock/dataNews.ts
@@ -0,0 +1,24 @@
+export type IDataNews = {
+  year: number;
+  title: string;
+};
+export const dataNews: IDataNews[] = [
+  {
+    year: 2015,
+    title: '13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды',
+  },
+  {
+    year: 2016,
+    title:
+      'Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11',
+  },
+  {
+    year: 2017,
+    title: 'Компания Tesla официально представила первый в мире электрический грузовик Tesla Semi',
+  },
+  {
+    year: 2018,
+    title:
+      'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem  ',
+  },
+];
