fef55da9a529f8fb28dfbe2107992b4d010464b2
A. Shustovsky <a.shustovsky@gmail.com>
2023-09-13 13:02:58 +0400

    refactor: add Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { TextSlider } from './components/TextSlider.tsx';
-import style from './App.module.scss';
-import { dataDates, dataEvents, dataNews } from './assets/mock/dataNews.ts';
-import { CircleSlider } from './components/CircleSlider.tsx';
+import { Section } from './components/Section.tsx';
+import { data } from './assets/mock/data.ts';
 
 export function App() {
   return (
     <>
-      <section className={style.historical}>
-        <h1 className={style.historical_header}>Исторические даты</h1>
-        <CircleSlider dataDates={dataDates} dataEvents={dataEvents} />
-        <TextSlider data={dataNews} />
-      </section>
+      <Section data={data} />
     </>
   );
 }
